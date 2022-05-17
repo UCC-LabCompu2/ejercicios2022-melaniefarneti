3c04884542187e4fd2251a98f913deda2d97068c
melaniefarneti <2113289@ucc.edu.ar>
2022-05-17 17:17:35 -0300

    .

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,21 +1,14 @@
-/**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
- */
 
-function cambiarUnidades(id, valor){
-    if(isNan(valor)){
+function cambiarUnidades(id, value){
+    if(isNan(value)){
         alert("se ingreso un valor invalido");
         document.lasUnidades.unid_metro.value = "";
         document.lasUnidades.unid_pulgada.value = "";
         document.lasUnidades.unid_pie.value = "";
         document.lasUnidades.unid_yarda.value = "";
     }else if(id== "metro"){
-        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
-        document.lasUnidades.unid_pie.value = 3.28084*valor;
-        document.lasUnidades.unid_yarda.value = 1.09361*valor;
+        document.lasUnidades.unid_pulgada.value = 39.3701*value;
+        document.lasUnidades.unid_pie.value = 3.28084*value;
+        document.lasUnidades.unid_yarda.value = 1.09361*value;
     }
 }
