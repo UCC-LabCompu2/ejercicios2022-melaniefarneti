b85409f542fcc8473e3a81483a9c27dd01f4c3d6
melaniefarneti <2113289@ucc.edu.ar>
2022-05-17 20:23:06 -0300

    agrego canvas de circulo y cuadrado git push

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -103,3 +103,15 @@ function cargarlocalstorage(){
     document.getElementById("dist").value = cantidad + " " + unidad;
 }
 
+function dibujarcircuad(){
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+    var margen = 5;
+    ctx.fillStyle="#333899";
+    ctx.fillRect(0+margen, yMax-40-margen, 40, 40);
+    ctx.arc(xMax/2, yMax/2, 20, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
