89b36ae727e9445902493035525e80c2c745ec46
melaniefarneti <2113289@ucc.edu.ar>
2022-05-17 17:27:16 -0300

    correccion de error en funcion isnan

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,14 +1,14 @@
 
-function cambiarUnidades(id, value){
-    if(isNan(value)){
+function cambiarUnidades(id, valor){
+    if(isNaN(valor)){
         alert("se ingreso un valor invalido");
         document.lasUnidades.unid_metro.value = "";
         document.lasUnidades.unid_pulgada.value = "";
         document.lasUnidades.unid_pie.value = "";
         document.lasUnidades.unid_yarda.value = "";
     }else if(id== "metro"){
-        document.lasUnidades.unid_pulgada.value = 39.3701*value;
-        document.lasUnidades.unid_pie.value = 3.28084*value;
-        document.lasUnidades.unid_yarda.value = 1.09361*value;
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.lasUnidades.unid_pie.value = 3.28084*valor;
+        document.lasUnidades.unid_yarda.value = 1.09361*valor;
     }
 }
